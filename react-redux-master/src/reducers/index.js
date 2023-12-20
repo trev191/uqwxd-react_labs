52abdb2e52bb28253180c50bfd0ab877f49f12a3
trev191 <trev191@yahoo.com>
2023-12-20 00:34:01 -0800

    Add redux functionality for global counter action, action creator, and reducer

--- react-redux-master/src/reducers/index.js
+++ react-redux-master/src/reducers/index.js
@@ -1,1 +1,14 @@
 //reducers
+import { combineReducers } from 'redux'
+
+const counter = (state=0, action) => {
+  if (action.type === 'INCREMENT') {
+    // increase value of counter by value passed to increment()
+    return state + action.inc;
+  }
+  return state;
+}
+
+const myReducers = combineReducers({counter});
+
+export default myReducers;
